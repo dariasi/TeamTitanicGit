13461279a79467f49d5e9598ecd8534bb8ce7ea7
liaoyi0 <tntprotoss@gmail.com>
2024-02-19 21:53:13 +0100

    Revert "Revert "update for task 1""
    This reverts commit 81a7bb974f25d1b485273e82dc3782dc806bc0c8.

--- titanic_analysis_prep.R
+++ titanic_analysis_prep.R
@@ -22,3 +22,26 @@ data$Embarked <- factor(data$Embarked)
 
 # Transform a Pclass variable into an ordered factor:
 data$Pclass <- factor(data$Pclass, levels = c(1, 2, 3), ordered = TRUE)
+
+#Imput missing values in the variable "Age"
+age_medians <- tapply(data$Age, data$title, median, na.rm = TRUE)
+data$Age <- ifelse(is.na(data$Age), age_medians[data$title], data$Age)
+
+#Extract Cabin information
+data$Side <- ifelse(as.integer(sub("\\D+", " ", data$Cabin)) %% 2 == 1, "Starboard", "Port")
+data$Deck <- gsub("([A-Za-z]+).*", "\\1", data$Cabin)
+data$Deck[data$Deck == ""] <- NA
+
+#Delete "Passenger ID", "Name", "Ticket" and "Cabin"
+data$PassengerId <- NULL
+data$Name <- NULL
+data$Ticket <- NULL
+data$Cabin <- NULL
+
+#save csv data
+write.csv(data, file = "data_cleaned.csv", row.names = FALSE)
+
+  
+  
+  
+  
